1a17681598e0ad5ace89f4d340939a4eeed7e731
fierdy_pandu <lifeisubw@gmail.com>
2020-01-11 14:47:43 +0700

    finish linked list

--- 10. LRU Cache/Hash Map & LRU Cache/lib/p04_linked_list.rb
+++ 10. LRU Cache/Hash Map & LRU Cache/lib/p04_linked_list.rb
@@ -16,6 +16,16 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+
+    ## rewiring phase
+    self.prev.next = self.next if self.prev
+    self.next.prev = self.prev if self.next
+    
+    ## removal/abandon phase 
+    self.prev = nil
+    self.next = nil
+
+    self
   end
 end
 
@@ -26,8 +36,8 @@ class LinkedList
   attr_reader :head, :tail
 
   def initialize
-    @head = Node.new("Original head")
-    @tail = Node.new("Original tail")
+    @head = Node.new(:head)
+    @tail = Node.new(:tail)
     @head.next = @tail
     @tail.prev = @head
   end
@@ -38,19 +48,11 @@ class LinkedList
   end
 
   def first
-    if empty?
-      nil
-    else
-      self.head.next
-    end
+    empty? ? nil : self.head.next
   end
 
   def last
-    if empty?
-      nil
-    else
-      self.tail.prev
-    end
+    empty? ? nil : self.tail.prev
   end
 
   def empty?
@@ -58,29 +60,50 @@ class LinkedList
   end
 
   def get(key)
+    each do |node|
+      if node.key == key
+        return node.val
+      end
+    end
   end
 
   def include?(key)
+    any? { |node| node.key == key }
   end
 
   def append(key, val)
     new_node = Node.new(key, val)
 
     self.tail.prev.next = new_node
-    new_node.next = self.tail
     new_node.prev = self.tail.prev
+    new_node.next = self.tail
     self.tail.prev = new_node
-
-    new_node
   end
 
   def update(key, val)
+    each do |node|
+      if node.key == key
+        node.val = val
+      end
+    end
   end
 
   def remove(key)
+    each do |node|
+      if node.key == key
+        node.remove
+        return node.val
+      end
+    end
+    nil
   end
 
   def each
+    current_node = self.head.next
+    until current_node == self.tail
+      yield current_node
+      current_node = current_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
@@ -91,6 +114,7 @@ end
 
 
 ll = LinkedList.new
-ll.append(:first, 1)
+ll.append("one", 1)
+ll.append("two", 2)
 
 p ll
